779643a23a16388856b923ff44176ff9fa482336
Raheem Kawojue <kawojue08@gmail.com>
2022-11-01 14:19:19 +0100

    setup font, icons, and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.jsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      colors: {
+        "pry-clr-0": 'rgb(241, 245, 248)',
+        "pry-clr-1": 'rgb(16, 42, 66)',
+        "pry-clr-2": 'rgb(73, 166, 233)',
+        "pry-clr-3": 'rgb(235, 247, 255)',
+        "pry-clr-4": 'rgb(6, 50, 81)',
+      }
+    },
   },
   plugins: [],
 }
